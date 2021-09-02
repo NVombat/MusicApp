31a10675d3ab58ef991f2af545ea8dbd7c1b9c10
NVombat <nvombatkere@gmail.com>
2021-09-02 15:08:16 +0530

    Django Server Creation And Setup

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { useContext } from 'react';
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import AuthPage from './Pages/AuthPage';
+import HomePage from './Pages/HomePage';
 import AuthContext from './store/auth-context';
 
 function App() {
