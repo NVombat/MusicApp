f2295e22858dd8fb57675cc77f5a052736bf69a8
Shivam Shekhar <shvam0000@gmail.com>
2021-09-04 21:26:15 +0530

    🍱 Icons Added

--- client/src/utils/icons/Index.tsx
+++ client/src/utils/icons/Index.tsx
@@ -4,3 +4,5 @@ export { default as Email } from './Email';
 export { default as Spinner } from './Spinner';
 export { default as Mic } from './Mic';
 export { default as Camera } from './Camera';
+export { default as Play } from './Play';
+export { default as Stop } from './Stop';
---
+++ client/src/utils/icons/Stop.tsx
@@ -0,0 +1,17 @@
+const Stop = () => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      stroke-width="0"
+      viewBox="0 0 16 16"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M5 3.5h6A1.5 1.5 0 0112.5 5v6a1.5 1.5 0 01-1.5 1.5H5A1.5 1.5 0 013.5 11V5A1.5 1.5 0 015 3.5z"></path>
+    </svg>
+  );
+};
+
+export default Stop;
