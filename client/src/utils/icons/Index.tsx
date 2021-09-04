56ffe9a7c20ffada99af6c81ef18527215df1a36
Shivam Shekhar <shvam0000@gmail.com>
2021-09-04 20:42:45 +0530

    🍱 Add Icons

--- client/src/utils/icons/Index.tsx
+++ client/src/utils/icons/Index.tsx
@@ -2,3 +2,5 @@ export { default as NavBurger } from './NavBurger';
 export { default as Phone } from './Phone';
 export { default as Email } from './Email';
 export { default as Spinner } from './Spinner';
+export { default as Mic } from './Mic';
+export { default as Camera } from './Camera';
