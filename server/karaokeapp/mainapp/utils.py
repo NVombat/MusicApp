2f7db3e0c865f0f669887f88952a97a4e037dc92
NVombat <nvombatkere@gmail.com>
2021-10-01 01:00:14 +0530

    API init:

--- server/karaokeapp/mainapp/utils.py
+++ server/karaokeapp/mainapp/utils.py
@@ -2,3 +2,19 @@ from django.http.response import JsonResponse
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import response
+
+
+def music_data_api(request, **kwargs):
+    try:
+        if request.GET[""]:
+            pass
+
+        else:
+            pass
+
+        return response.JsonResponse(data="Test", status=status.HTTP_200_OK)
+
+    except Exception as e:
+        return response.JsonResponse(
+            {"error": "Error Occured"}, status=status.HTTP_400_BAD_REQUEST
+        )
