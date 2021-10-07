ce7de1c61ee6de960410b891ff1a2a586437e3f5
NVombat <nvombatkere@gmail.com>
2021-10-08 02:38:41 +0530

    :construction: api and path fix

--- server/core/urls.py
+++ server/core/urls.py
@@ -20,9 +20,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("mainapp.urls")),
+    path("api/", include("mainapp.urls")),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
--- server/mainapp/urls.py
+++ server/mainapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import Upload
 
 urlpatterns = [
-    path("upload", Upload, Upload.as_view()),
+    path("upload", Upload.as_view()),
 ]
