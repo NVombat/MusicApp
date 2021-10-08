4a2d752403e2f75fd41397046e197efad27dc688
NVombat <nvombatkere@gmail.com>
2021-10-09 01:47:10 +0530

    :construction: api partial fix: data issue

--- server/mainapp/views.py
+++ server/mainapp/views.py
@@ -1,33 +1,41 @@
 from django.http.response import JsonResponse
 from rest_framework.views import APIView
-from django.shortcuts import render
 from rest_framework import status
 from django.http import response
 import requests
 
 from .utils import recv_music_data, send_music_data
-from mainapp import Music_Data
 
 
 class Data(APIView):
     def post(self, request, **kwargs) -> JsonResponse:
-        """
-        When User Uploads -> Data is received via POST request
+        """Receiving user uploaded data via POST requests
+
+        Args:
+            request ([type])
+
+        Returns:
+            JsonResponse
         """
 
         print("Receiving Data API")
 
-        data = recv_music_data(request)
+        data = recv_music_data(request, **kwargs)
 
         return data
 
     def get(self, request, **kwargs) -> JsonResponse:
-        """
-        When User Requests Data -> Data is sent via JsonResponse
+        """Sending user data when hit with GET requests
+
+        Args:
+            request ([type])
+
+        Returns:
+            JsonResponse
         """
 
         print("Sending Data API")
 
-        data = send_music_data(request)
+        data = send_music_data(request, **kwargs)
 
         return data
