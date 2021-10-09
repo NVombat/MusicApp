8268717efaf9ae026454457caff019ffcbe158f8
NVombat <nvombatkere@gmail.com>
2021-10-10 01:20:24 +0530

    :construction_worker: CI fix

--- server/run_tests.sh
+++ server/run_tests.sh
@@ -5,7 +5,7 @@ echo "[STARTING-DJANGO-SERVER]"
 python3 manage.py runserver &
 
 sleep 5
-python3 -m unittest server/tests/test_app.py
+python3 -m unittest
 
 # sleep $t
 # python3 -m unittest server/tests/.py
