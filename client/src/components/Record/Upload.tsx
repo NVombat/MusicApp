6ea1426cfcaf30cedc056afb5f5ce612197b0eb7
Shivam Shekhar <shvam0000@gmail.com>
2021-10-15 01:33:25 +0530

    🚧  File Upload Fixed

--- client/src/components/Record/Upload.tsx
+++ client/src/components/Record/Upload.tsx
@@ -1,4 +1,37 @@
+import axios from 'axios';
+
+const url = 'http://localhost:8000/api/uploads';
+
+enum Inputs {
+  Name = 'Name',
+  Email = 'Email',
+  File = 'File',
+}
+
 const Upload = () => {
+  const submitForm = (e: React.FormEvent) => {
+    e.preventDefault();
+    let _name = (e.target as HTMLFormElement)[Inputs.Name].value;
+    let _email = 'abc@gmail.com';
+    let _file = (e.target as HTMLFormElement)[Inputs.File].files[0];
+    const finalFormData = new FormData();
+
+    finalFormData.append('Name', _name);
+    finalFormData.append('Email', _email);
+    finalFormData.append('File', _file);
+    finalFormData.append('Filename', _file.name);
+
+    axios
+      .post(url, finalFormData)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err, err?.response);
+        console.log('messed up in catch block');
+      });
+  };
+
   return (
     <div>
       <h1 className="flex justify-center px-2 font-bold text-xl my-10">
@@ -9,26 +42,24 @@ const Upload = () => {
           <div className="card bg-blue-400 shadow-lg  w-full h-full rounded-3xl absolute  transform -rotate-6"></div>
           <div className="card bg-red-400 shadow-lg  w-full h-full rounded-3xl absolute  transform rotate-6"></div>
           <div className="relative w-full rounded-3xl  px-6 py-4 bg-gray-100 shadow-md">
-            <label
-              htmlFor=""
-              className="block mt-3 text-xl font-bold text-gray-700 text-center"
-            >
+            <label className="block mt-3 text-xl font-bold text-gray-700 text-center">
               Upload Media
             </label>
-            <form className="mt-10">
+            <form className="mt-10" onSubmit={submitForm}>
               <div>
-                <label htmlFor="Email">Name</label>
+                <label>Name</label>
                 <input
+                  name={Inputs.Name}
                   type="text"
                   placeholder="John Doe"
                   className="mt-1 px-4 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0 focus:outline-none"
-                  required
                 />
               </div>
               <div className="mt-7">
                 <div>
-                  <label htmlFor="upload-file">Choose File</label>
+                  <label>Choose File</label>
                   <input
+                    name={Inputs.File}
                     type="file"
                     placeholder="John Doe"
                     className="mt-1 px-4 block"
@@ -37,7 +68,10 @@ const Upload = () => {
                 </div>
               </div>
               <div className="mt-7">
-                <button className="bg-blue-500 w-full py-3 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+                <button
+                  type="submit"
+                  className="bg-blue-500 w-full py-3 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105"
+                >
                   Upload
                 </button>
               </div>
