a46e5274dcb37216fc7df11c26c62c714ced9766
Shivam Shekhar <shvam0000@gmail.com>
2021-10-15 20:18:49 +0530

    🚧  Add user name and email on profile page

--- client/src/components/Profile/Hero.tsx
+++ client/src/components/Profile/Hero.tsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import ResetPassword from './ResetPasswordForm';
+
 const Hero = () => {
+  const [userData, setUserData] = useState<any>([]);
+
+  useEffect(() => {
+    axios
+      .post(`${process.env.REACT_APP_GET_USER}`, {
+        idToken: localStorage.getItem('token'),
+      })
+      .then((res) => {
+        //@ts-ignore
+        setUserData(res.data.users);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
+      <div>
+        {
+          //@ts-ignore
+          userData.map((item, index) => (
+            <ol key={index}>
+              <li>{item.displayName}</li>
+              <li>{item.email}</li>
+            </ol>
+          ))
+        }
+      </div>
       <ResetPassword />
     </div>
   );
