b79b4f1bda304d92371f291ea7cc5ff5885a7c2b
Shivam Shekhar <shvam0000@gmail.com>
2021-10-15 12:01:07 +0530

    🚧 Fetch all the posts from DB

--- client/src/components/Posts/Hero.tsx
+++ client/src/components/Posts/Hero.tsx
@@ -1,6 +1,40 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const url = 'http://localhost:8000/api/posts';
+
 const Hero = () => {
+  const [posts, setPosts] = useState<any>([]);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        console.log('axios response', res.data);
+        console.log('type of axios data', typeof res.data);
+        setPosts(res.data);
+      })
+      .catch((err) => console.log(err, err?.response));
+  }, []);
+
   return (
-    <div className="flex justify-center mt-5 text-2xl font-semibold">Posts</div>
+    <div>
+      <span className="flex justify-center mt-5 text-2xl font-semibold">
+        Posts
+      </span>
+
+      {
+        //@ts-ignore
+        posts.map((item, index) => (
+          <li key={index}>
+            <ol>{item.Name}</ol>
+            <ol>{item.Email}</ol>
+            <ol>{item.Filename}</ol>
+            <ol>{item.CloudFilename}</ol>
+          </li>
+        ))
+      }
+    </div>
   );
 };
 
