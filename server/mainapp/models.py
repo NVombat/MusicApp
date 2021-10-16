8ea3949611fd11c6be399d5f85c6a0a7b62cb47b
NVombat <nvombatkere@gmail.com>
2021-10-16 22:42:42 +0530

    :fire: lib cleanup & requirement update

--- server/mainapp/models.py
+++ server/mainapp/models.py
@@ -1,4 +1,3 @@
-from django.http.response import JsonResponse
 from django.http import response
 from dotenv import load_dotenv
 import pymongo
--- server/mainapp/utils.py
+++ server/mainapp/utils.py
@@ -1,5 +1,3 @@
-from django.http.response import JsonResponse
-from rest_framework.response import Response
 from rest_framework import status
 from django.http import response
 from dotenv import load_dotenv
