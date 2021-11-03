d8cba5f792d973af2bcc5707b0e4369170f06f8f
NVombat <nvombatkere@gmail.com>
2021-11-04 04:46:11 +0530

    :rotating_light: lint fix

--- server/authentication/views.py
+++ server/authentication/views.py
@@ -3,11 +3,13 @@ from django.http import response
 
 from core.throttle import throttle
 
+
 class Login(APIView):
     throttle_classes = [throttle]
 
     def post(self, request, **kwargs) -> response.JsonResponse:
         pass
+
 
 class ResetPassword(APIView):
     throttle_classes = [throttle]
