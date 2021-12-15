4d2f08a0f4f04cf86e2186e8146e4e3dae4f2903
Shivam Shekhar <shvam0000@gmail.com>
2021-12-15 10:55:36 +0530

    ⚡️ More Languages to record in

---
+++ client/src/components/Record/Songs/English.tsx
@@ -0,0 +1,17 @@
+const English = () => {
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/DvmQzFFLY1c"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default English;
---
+++ client/src/components/Record/Songs/Tamil.tsx
@@ -0,0 +1,11 @@
+const Tamil = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="transform transition animate-bounce text-4xl my-10 font-semibold">
+        Coming Soon...
+      </h1>
+    </div>
+  );
+};
+
+export default Tamil;
