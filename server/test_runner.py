9ee1d34f137ea154b52fe18e291f84c75932f4ee
NVombat <nvombatkere@gmail.com>
2022-01-26 14:09:06 +0530

    :memo: init tests for auth, admins and apis

--- server/test_runner.py
+++ server/test_runner.py
@@ -4,6 +4,10 @@ from authentication.tests import (
     test_models,
     test_jwt,
 )
+from admins.tests import (
+    test_models,
+    test_auth,
+)
 from tests import (
     test_models,
     test_apis,
@@ -13,12 +17,13 @@ from tests import (
 def get_unittests(suite):
     suite.addTest(unittest.makeSuite(test_jwt.Test_JWT))
     suite.addTest(unittest.makeSuite(test_models.Test_Auth_Model))
+    # suite.addTest(unittest.makeSuite(test_jwt.))
+    # suite.addTest(unittest.makeSuite(test_models.))
 
 
 def get_server_tests(suite):
-    suite.addTest(unittest.makeSuite(test_models.TestModels))
     suite.addTest(unittest.makeSuite(test_apis.TestAPI))
-
+    suite.addTest(unittest.makeSuite(test_models.TestModels))
 
 def main():
     suite = unittest.TestSuite()
