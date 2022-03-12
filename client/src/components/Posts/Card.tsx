fd23bffc1207a67aa131482ef36a8b4f3d65dd69
Thejaswin <thejas2002@yahoo.in>
2022-03-12 11:26:36 +0530

    added download icon for audio/video

--- client/src/components/Posts/Card.tsx
+++ client/src/components/Posts/Card.tsx
@@ -16,15 +16,12 @@ const Card: React.FC<CardProps> = ({
 }: CardProps) => {
   return (
     <div className="flex items-center flex-col border-2 border-black px-5 py-1 my-3 w-80 ">
-      {/* <a href={link}>
-        <div className="px-2 text-3xl">{downloadIcon}</div>
-      </a> */}
       
-      {/* added react player to preview audio and video files */}
       <ReactPlayer url={link} controls width={300} height={100} light='https://media.istockphoto.com/photos/sound-wave-picture-id1287065554?b=1&k=20&m=1287065554&s=170667a&w=0&h=6JIPYTu98DAXdChKSeu-Td3zv8KyLC3yhu-rWfkDQQc='  />
-      
-
       <div className="text-sm text-center mt-2 font-semibold">{filename}</div>
+      <a href={link}>
+        <div className="px-2 text-3xl">{downloadIcon}</div>
+      </a>
     </div>
   );
 };
