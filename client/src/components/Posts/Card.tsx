5d9e6a15d039ed76b20c990db09fca0d31e3850c
--global <--global>
2022-03-14 22:17:42 +0530

    :sparkles: add media player for the posts page

--- client/src/components/Posts/Card.tsx
+++ client/src/components/Posts/Card.tsx
@@ -1,7 +1,6 @@
-import { url } from 'inspector';
 import React from 'react';
 
-import ReactPlayer from 'react-player'
+import ReactPlayer from 'react-player';
 
 interface CardProps {
   link: string;
@@ -16,8 +15,13 @@ const Card: React.FC<CardProps> = ({
 }: CardProps) => {
   return (
     <div className="flex items-center flex-col border-2 border-black px-5 py-1 my-3 w-80 ">
-      
-      <ReactPlayer url={link} controls width={300} height={100} light='https://media.istockphoto.com/photos/sound-wave-picture-id1287065554?b=1&k=20&m=1287065554&s=170667a&w=0&h=6JIPYTu98DAXdChKSeu-Td3zv8KyLC3yhu-rWfkDQQc='  />
+      <ReactPlayer
+        url={link}
+        controls
+        width={300}
+        height={100}
+        light="https://media.istockphoto.com/photos/sound-wave-picture-id1287065554?b=1&k=20&m=1287065554&s=170667a&w=0&h=6JIPYTu98DAXdChKSeu-Td3zv8KyLC3yhu-rWfkDQQc="
+      />
       <div className="text-sm text-center mt-2 font-semibold">{filename}</div>
       <a href={link}>
         <div className="px-2 text-3xl">{downloadIcon}</div>
