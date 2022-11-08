12e4a72268ee3720d34080e3d7411857cef17966
Jakub Vereb <jakub.vereb@kiwi.com>
2022-11-08 19:42:55 +0100

    feat: created models for likes and comments

--- server/mainapp/models.py
+++ server/mainapp/models.py
@@ -123,3 +123,167 @@ class MusicData:
             return docs
 
         raise DataFetchingError("There Are No Posts In The Database At This Moment")
+
+
+class Likes:
+    def __init__(self) -> None:
+        """
+        Connect to MongoDB
+        """
+        client = pymongo.MongoClient(DATABASE["mongo_uri"])
+        self.db = client[DATABASE["db"]]["Likes"]
+
+    def insert_data(
+        self,
+        music_data_pid: str,
+        user_email: str,
+    ) -> None:
+        """Insert file name and data into db
+
+        Args:
+            music_data_pid: FK for pid of the post
+            user_email: FK for user email
+
+        Returns:
+            None
+        """
+        if self.db.find_one({"music_data_pid": music_data_pid, "user_email": user_email}):
+            raise FileAlreadyExistsForCurrentUserError(
+                f"Like for post {music_data_pid} Already Exists For User {user_email}"
+            )
+
+        data = {
+            "music_data_pid": music_data_pid,
+            "user_email": user_email,
+        }
+        self.db.insert_one(data)
+
+    def delete_data(self, music_data_pid: str, user_email: str) -> None:
+        """Delete file and related data from db
+
+        Args:
+            music_data_pid: User Email ID
+            user_email: Name of File
+
+        Returns:
+            None
+        """
+        if self.db.find_one(
+            {
+                "music_data_pid": music_data_pid,
+                "user_email": user_email,
+            }
+        ):
+            self.db.delete_one(
+                {
+                    "music_data_pid": music_data_pid,
+                    "user_email": user_email,
+                },
+            )
+        else:
+            raise FileDoesNotExistForCurrentUserError(
+                f"Like for post {music_data_pid} does not exists for user {user_email}"
+            )
+
+    def fetch_data(self) -> response.JsonResponse:
+        """Fetches all data from db
+
+        Args:
+            None
+
+        Returns:
+            response.JsonResponse
+        """
+        if data := self.db.find(
+            {},
+            {
+                "_id": 0,
+            },
+        ).sort("music_data_pid", -1):
+            likes = list(data)
+            # docs.append({"success_status": True})
+            # json_data = response.JsonResponse(docs, safe=False)
+            # return json_data
+            return likes
+
+        raise DataFetchingError("There Are No Likes In The Database At This Moment")
+
+
+class Comments:
+    def __init__(self) -> None:
+        """
+        Connect to MongoDB
+        """
+        client = pymongo.MongoClient(DATABASE["mongo_uri"])
+        self.db = client[DATABASE["db"]]["Comments"]
+
+    def insert_data(
+        self,
+        music_data_pid: str,
+        user_email: str,
+        comment: str,
+    ) -> None:
+        """Insert file name and data into db
+
+        Args:
+            music_data_pid: FK for pid of the post
+            user_email: FK for user email
+            comment: comment
+
+        Returns:
+            None
+        """
+
+        data = {
+            "music_data_pid": music_data_pid,
+            "user_email": user_email,
+            "comment": comment,
+        }
+        self.db.insert_one(data)
+
+    def delete_data(self, comment_id: str) -> None:
+        """Delete file and related data from db
+
+        Args:
+            comment_id: comment ID
+
+        Returns:
+            None
+        """
+        if self.db.find_one(
+            {
+                "_id": comment_id,
+            }
+        ):
+            self.db.delete_one(
+                {
+                    "_id": comment_id,
+                },
+            )
+        else:
+            raise FileDoesNotExistForCurrentUserError(
+                f"Comment with id {comment_id} does not exists."
+            )
+
+    def fetch_data(self) -> response.JsonResponse:
+        """Fetches all data from db
+
+        Args:
+            None
+
+        Returns:
+            response.JsonResponse
+        """
+        if data := self.db.find(
+            {},
+            {
+                "_id": 0,
+            },
+        ).sort("music_data_pid", -1):
+            comments = list(data)
+            # docs.append({"success_status": True})
+            # json_data = response.JsonResponse(docs, safe=False)
+            # return json_data
+            return comments
+
+        raise DataFetchingError("There Are No Likes In The Database At This Moment")
